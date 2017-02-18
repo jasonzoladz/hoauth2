c73c5ae8d5370d46ad9e441807a2786bfd495f2f
HaishengWu <freizl@gmail.com>
2017-02-17 17:08:08 -0800

    create nix file

---
+++ hoauth2.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, aeson, base, bytestring, http-conduit, http-types
+, stdenv, text, unordered-containers
+}:
+mkDerivation {
+  pname = "hoauth2";
+  version = "0.5.8";
+  buildInputs = [ zlib ]
+  sha256 = "0q0fh85cmshpgng8mzdrwl0wkasdrbl8zhvqrg8yjpa5b49frb6a";
+  libraryHaskellDepends = [
+    aeson base bytestring http-conduit http-types text
+    unordered-containers
+  ];
+  homepage = "https://github.com/freizl/hoauth2";
+  description = "Haskell OAuth2 authentication client";
+  license = stdenv.lib.licenses.bsd3;
+}
