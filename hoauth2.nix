7c0353b4e78be5c2b143da951ff4df24843fe7f9
HaishengWu <freizl@gmail.com>
2017-02-17 17:33:01 -0800

    wip

--- hoauth2.nix
+++ hoauth2.nix
@@ -1,10 +1,9 @@
 { mkDerivation, aeson, base, bytestring, http-conduit, http-types
-, stdenv, text, unordered-containers
+, stdenv, text, unordered-containers, zlib
 }:
 mkDerivation {
   pname = "hoauth2";
   version = "0.5.8";
-  buildInputs = [ zlib ]
   sha256 = "0q0fh85cmshpgng8mzdrwl0wkasdrbl8zhvqrg8yjpa5b49frb6a";
   libraryHaskellDepends = [
     aeson base bytestring http-conduit http-types text
